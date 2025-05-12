16e6800c0734df7489e09e0d1f0082b358ab33ac
Applepielord291 <pixeldash2007@gmail.com>
2025-05-12 10:58:27 -0400

    project

--- labs/Lab6/ShellSort/src/ShellSort.java
+++ labs/Lab6/ShellSort/src/ShellSort.java
@@ -1,6 +1,12 @@
+/* Nigel Garcia
+ * May 12 2025
+ * ShellSort
+ * sorts a 2d array using the shellsort method
+ */
+
 public class ShellSort {
     public static void main(String[] args) throws Exception 
     {
-        
+        int[][] num = {{5, 16, 7, 8}, {25, 46, 67, 88}, {79, 110, 121, 12}};
     }
 }
