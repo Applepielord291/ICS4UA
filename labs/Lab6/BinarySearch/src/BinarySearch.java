6e2ddd3c226361764508498846b398f4352fd45b
Applepielord291 <pixeldash2007@gmail.com>
2025-05-15 12:52:43 -0400

    project

---
+++ labs/Lab6/BinarySearch/src/BinarySearch.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+/* Nigel Garcia
+ * May 15 2025
+ * Binary Search
+ * searches for a number using binary search
+ */
+
+public class BinarySearch {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a number you want to search for: "); //init
+        int res = scn.nextInt();
+        int index = 0;
+        int[][] num = {{5, 16, 7, 8}, {25, 46, 67, 88}, {79, 110, 121, 12}};
+
+        int[] realNums = new int[num.length * num[0].length]; //conversion
+        for (int i = 0; i < num.length; i++)
+        {
+            for (int j = 0; j < num[i].length; j++)
+            {
+                realNums[index] = num[i][j];
+                index++;
+            }
+        }
+        scn.close();
+
+        realNums = Quick(realNums, 0, realNums.length - 1); //results
+        int found = binarySearch(realNums, 0, 0, realNums.length - 1, res);
+        if (found != 0) System.out.println("Number " + found + " found.");
+        else System.out.println("Number " + res + " not found.");
+    }
+    public static int[] Quick(int[] num, int lowLim, int highLim) //quicksort
+    {
+        int i = lowLim; int j = highLim; int h;
+        int x = num[(lowLim + highLim) / 2];
+        do
+        {
+            while (num[i] < x) i++;
+            while (num[j] > x) j--;
+            if (i <= j)
+            {
+                h = num[i]; 
+                num[i] = num[j]; 
+                num[j] = h;
+                i++; j--;
+            }
+        } while (i <= j);
+
+        if (lowLim < j) Quick(num, lowLim, j);
+        if (i < highLim) Quick(num, i, highLim);
+        return num;
+    }
+    public static int binarySearch(int[] num, int first, int mid, int last, int res) //binary search
+    {
+        /*for (int i = 0; i < num.length; i++) //rows 
+        {
+            int last = num[i].length - 1;       !!KEEPING THE 2D ARRAY BINARY SEARCH VERSION JUST IN CASE!!
+            while (first <= last) //cols
+            {
+                System.out.println(mid);
+                if (num[i][mid] == res) //found
+                {
+                    return num[i][mid];
+                }
+                else if (num[i][mid] > res) last = mid-1; //eliminate right half
+                else if (num[i][mid] < res) first = mid+1; //eliminate left half
+                mid = first + (last - first)/2;
+            }
+            first = 0; mid = 0;
+        }*/
+        while (first <= last) //cols
+        {
+            if (num[mid] == res) //found
+            {
+                return num[mid];
+            }
+            else if (num[mid] > res) last = mid-1; //eliminate right half
+            else if (num[mid] < res) first = mid+1; //eliminate left half
+            mid = first + (last - first)/2;
+        }
+        return 0;
+    }
+}
---
+++ labs/Lab6/LinearSearch/src/LinearSearch.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/* Nigel Garcia
+ * May 15 2025
+ * Linear search
+ * searches for a number using the linear search method (The array is already sorted using the quick sort method)
+ * was not aware, the array must be sorted first to work
+ */
+
+public class LinearSearch {
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int[][] num = {{5, 16, 7, 8}, {25, 46, 67, 88}, {79, 110, 121, 12}};
+        int pos = 1;
+        System.out.println("Enter the number you want to search for");
+        int res = scn.nextInt();
+        for (int i = 0; i < num.length; i++) 
+        {
+            for (int j = 0; j < num.length + 1; j++) 
+            {
+                pos++;
+                if (num[i][j] == res) 
+                {
+                    System.out.println("Number found " + res + " at location " + (pos-1));
+                    break;
+                }
+            }
+        }
+        scn.close();
+    }
+}
